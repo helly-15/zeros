f2402a2170e755d53fa7ffb2afacc603da4cd20b
Lenocka <lvutina.en@gmail.com>
2019-10-02 16:08:35 +0300

    fix:8 tests pass

--- src/index.js
+++ src/index.js
@@ -14,19 +14,23 @@ module.exports =
     //console.log(" length of element "+elementLength);
     if (factorialArray[i].substr(-1,1) !=factorialArray[i].substr(-2,1)){
       //console.log(" element "+ factorialArray[i]);
-      //console.log (factorialArray[i].substr(-2,2));
+      //console.log (factorialArray[i].substr(-2,1));
       for (let j=+factorialArray[i].slice(0,elementLength-1);j>0;j--){
         if (j%2==0&&j%5!=0){
           amountOf2=amountOf2+j/2;
         }
-        if (j%5==0){
+        if (j%5==0&&j%2!=0){
           amountOf5=amountOf5+j/5;
+        }
+        if (j%10==0){
+          numOfZeros++;
+
         }
       }
 
     }
     //work with !! signs
-    else {//console.log (+factorialArray[i].slice(0,elementLength-2));
+    else {//console.log ( "Double explanation"+ +factorialArray[i].slice(0,elementLength-2));
       for (let j=+factorialArray[i].slice(0,elementLength-2);j>0;j=j-2){
         
         if (j%2==0&&j%5!=0){
@@ -43,7 +47,7 @@ module.exports =
 
     }
   }
-  
+  //console.log("num of zeroes now "+numOfZeros)
   if (amountOf2>amountOf5){
      numOfZeros+=amountOf5;
   }
