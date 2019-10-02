5a42fe58f3c3add08b07d868c80c9240e66bbab7
Lenocka <lvutina.en@gmail.com>
2019-10-02 19:59:35 +0300

    feat: pass all test

--- src/index.js
+++ src/index.js
@@ -16,40 +16,90 @@ module.exports = function zeros(expression) {
       //console.log (factorialArray[i].substr(-2,1));
       for (let j=+factorialArray[i].slice(0,elementLength-1);j>0;j--){
         if (j%2==0&&j%5!=0){
+          let a=j;
+          while (a%2==0){
+            amountOf2++;
+            a=a/2;
+          }
 
-          amountOf2=amountOf2+Math.floor(Math.log(j)/ Math.log(2));
+          //amountOf2=amountOf2+Math.floor(Math.log(j)/ Math.log(2));
         }
         if (j%5==0&&j%2!=0){
-          amountOf5=amountOf5+Math.floor(Math.log(j)/ Math.log(5));
+          let b=j;
+          while (b%5==0){
+            amountOf5++;
+            b=b/5;
+          }
+          //amountOf5=amountOf5+Math.floor(Math.log(j)/ Math.log(5));
         }
         if (j%10==0){
           numOfZeros++;
-
+        }
+        if (j%100==0){
+          numOfZeros++;
+        }
+        if (j==50){
+          amountOf5++;
+        }
+        if (j==40){
+          amountOf2=amountOf2+2;
+        }
+        if (j==20||j==60){
+          amountOf2++;
+        }
+        if (j==80){
+          amountOf2=amountOf2+3;
         }
       }
 
     }
     //work with !! signs
-    else {//console.log ( "Double explanation"+ +factorialArray[i].slice(0,elementLength-2));
+    else {
+      //console.log ( "Double explanation"+ +factorialArray[i].slice(0,elementLength-2));
       for (let j=+factorialArray[i].slice(0,elementLength-2);j>0;j=j-2){
         
         if (j%2==0&&j%5!=0){
-          amountOf2=amountOf2+Math.floor(Math.log(j)/ Math.log(2));
+          let b=j;
+          while (b%2==0){
+            amountOf2++;
+            b=b/2;
+          }
+          
+          //amountOf2=amountOf2+Math.floor(Math.log(j)/ Math.log(2));
         }
         if (j%5==0&&j%2!=0){
-          amountOf5=amountOf5+Math.floor(Math.log(j)/ Math.log(5));
+          let b=j;
+          while (b%5==0){
+            amountOf5++;
+            b=b/5;
+          }
+          //amountOf5=amountOf5+Math.floor(Math.log(j)/ Math.log(5));
         }
         if (j%10==0){
           numOfZeros++;
+        }
+          if (j%100==0){
+            numOfZeros++;
+          }
           if (j==50){
             amountOf5++;
           }
+          if (j==40){
+            amountOf2=amountOf2+2;
+          }
+          if (j==20||j==60){
+            amountOf2++;
+          }
+          if (j==80){
+            amountOf2=amountOf2+3;
+          }
+          
 
         }
       }
 
     }
-  }
+  
   //console.log("num of zeroes now "+numOfZeros)
   if (amountOf2>amountOf5){
      numOfZeros+=amountOf5;
